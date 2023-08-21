ca19dad584209e0addd0092a0444e185810ca905
Benjamin <auco0023@algonquinlive.com>
2023-08-21 19:19:29 -0400

    added jquery navbar

--- Scripts.js
+++ Scripts.js
@@ -1,32 +1,7 @@
-const projE = document.getElementById("projectsNav");
-const contactE = document.getElementById("contactNav");
-const coursesE = document.getElementById("coursesNav");
-const bodyE = document.querySelector("body");
+$( "#navBar" ).load( "navbar.html" );
+
 const containerE = document.querySelector(".container");
 const courseContainerE = document.querySelector(".coursesContainer")
-
-
-function hoverEffects(navE, tohover, fromhover){
-    navE.addEventListener("mouseover", (e) =>{ 
-        bodyE.classList = "animationDetails noscroll backgroundSky"
-        bodyE.classList.remove(fromhover);
-        bodyE.classList.toggle(tohover);
-     
-    })
-    
-    navE.addEventListener("mouseout", (e) =>{ 
-        bodyE.classList.add(fromhover);
-        bodyE.classList.toggle(tohover);
-    
-    })
-}
-
-hoverEffects(projE, "backgroundToWireframes", "backgroundFromWireframes");
-hoverEffects(coursesE, "backgroundToSchool", "backgroundFromSchool");
-
-
-
-
 
 function addCourses(){
     console.log(courses);
@@ -80,3 +55,4 @@ function addCourses(){
 }
 
 addCourses();
+
---
+++ animations.js
@@ -0,0 +1,26 @@
+const projE = document.getElementById("projectsNav");
+const contactE = document.getElementById("contactNav");
+const coursesE = document.getElementById("coursesNav");
+const bodyE = document.querySelector("body");
+
+
+
+
+function hoverEffects(navE, tohover, fromhover){
+    navE.addEventListener("mouseover", (e) =>{ 
+        bodyE.classList = "animationDetails noscroll backgroundSky"
+        bodyE.classList.remove(fromhover);
+        bodyE.classList.toggle(tohover);
+     
+    })
+    
+    navE.addEventListener("mouseout", (e) =>{ 
+        bodyE.classList.add(fromhover);
+        bodyE.classList.toggle(tohover);
+    
+    })
+}
+
+hoverEffects(projE, "backgroundToWireframes", "backgroundFromWireframes");
+hoverEffects(coursesE, "backgroundToSchool", "backgroundFromSchool");
+
