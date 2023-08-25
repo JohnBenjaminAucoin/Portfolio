7528c54e0af47909424af51a99387f96a3cb1769
Benjamin <auco0023@algonquinlive.com>
2023-08-25 15:57:56 -0400

    fabrica preview fix

--- projects.js
+++ projects.js
@@ -11,7 +11,7 @@ const projects = [
         "description":"A design for a social media website intended for creatives. I was responsible for the home page and pages labeled Ben 'number'. I also designed the logo in Adobe Illustrator. I really enjoyed designing the cards and the hover effects.",
         "link":"https://www.figma.com/file/lTq4zhaTMKNKmZlzRU3Udc/All-parts-%2F-correct-file-structure?type=design&node-id=404%3A1180&mode=design&t=cn52X1zBfgrVEjQb-1",
         "type":"Academic",
-        "image":"FabricaPreview.jpg"
+        "image":"fabricapreview.jpg"
     }
 
 ]
